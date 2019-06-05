207dbbff7bf8aa0a384a5a724643ed7e0688125f
MitchellGerdisch <mitchell.gerdisch@jesslex.com>
2019-06-05 14:56:02 -0400

    bug fix

--- sundry_hacks/Get_RIs_from_Chrome_Console.js
+++ sundry_hacks/Get_RIs_from_Chrome_Console.js
@@ -1,6 +1,7 @@
 # Documented in Green Circle article, DOC-6375
 # If this script is changed, be sure to update the article.
 
+no_action_found = true;
 fetch('/vmturbo/rest/markets/Market/actions').then(res => {
 	return res.json();
 }).then(actions => {
@@ -8,15 +9,21 @@ fetch('/vmturbo/rest/markets/Market/actions').then(res => {
 		new_coverage = 0;
 		return_line = "";
 		try {
-			coupon_value = record.reservedInstance.coupons.value;
-			coupon_avg = record.reservedInstance.coupons.capacity.avg;
-			new_coverage = Math.round((coupon_value/coupon_avg)*100);
-			if (new_coverage > 0) {
-				console.log("Instance Name: "+ record.target.displayName); 
-				console.log("- RI Utilization: " + new_coverage + "%");
-				console.log("- Action: " + record.details);
-				console.log("- Reason: " + record.risk.subCategory);
+			if (record.target.className == "VirtualMachine") {
+				coupon_value = record.reservedInstance.coupons.value;
+				coupon_avg = record.reservedInstance.coupons.capacity.avg;
+				new_coverage = Math.round((coupon_value/coupon_avg)*100);
+				if (new_coverage > 0) {
+					no_action_found = false;
+					console.log("Instance Name: "+ record.target.displayName); 
+					console.log("- RI Utilization: " + new_coverage + "%");
+					console.log("- Action: " + record.details);
+					console.log("- Reason: " + record.risk.subCategory);
+				}
 			}
 		} catch(err) {} 
 	});
+	if (no_action_found) {
+		console.log("*** NO RI UTILIZATION IMPROVING ACTIONS FOUND. ***")
+	}
 });
