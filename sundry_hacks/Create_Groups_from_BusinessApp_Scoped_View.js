8bef9b21380ef627b6206fea96ab41dffe17a2a9
MitchellGerdisch <mitchell.gerdisch@jesslex.com>
2019-11-13 16:09:50 -0600

    tweaks to output

--- sundry_hacks/Create_Groups_from_BusinessApp_Scoped_View.js
+++ sundry_hacks/Create_Groups_from_BusinessApp_Scoped_View.js
@@ -25,7 +25,7 @@ function CreateUpdate_Group(group_url, api_method, body) {
 	})
 }
 
-
+/* MAIN */
 var thisurl = document.URL;
 /* console.log("URL: "+thisurl); */
 var url_array = thisurl.split('/');
@@ -34,13 +34,13 @@ var scope_id = url_array[url_array.length-1];
 
 /* Walk through the view and get/create the following items:
  * business application name used to create the groups
- * list of VMs in the business application
- * list of DB servers in the business applicaiton
+ * array of VMs in the business application
+ * array of DB servers in the business applicaiton
  */
 
 var ba_name = "BA NAME NOT FOUND"
-var static_filter_ba_vms = []
-var static_filter_ba_dbs = []
+var ba_vms = []
+var ba_dbs = []
 
 fetch('/vmturbo/rest/search/?q=&scopes='+scope_id).then(res => {
 	return res.json();
@@ -51,33 +51,31 @@ fetch('/vmturbo/rest/search/?q=&scopes='+scope_id).then(res => {
 			ba_name = record.displayName
 		} 
 		else if (record.className == "VirtualMachine") {
-			static_filter_ba_vms.push(record.uuid)
+			ba_vms.push(record.uuid)
 		} else if (record.className == "DatabaseServer") {
-			static_filter_ba_dbs.push(record.uuid)
+			ba_dbs.push(record.uuid)
 		}
 
 	});
 	console.log("Business Application Name: "+ba_name)
-	console.log("VMs in Bus App: "+static_filter_ba_vms)
-	console.log("DB Servers in Bus App: "+static_filter_ba_dbs)
-	/* Build the vm and db groups for the application
-	 */
+	console.log("VMs in Bus App: " + ((ba_vms.length > 0) ? ba_vms : "NONE FOUND"))
+	console.log("DB Servers in Bus App: " + ((ba_dbs.length > 0) ? ba_dbs : "NONE FOUND"))
 	
 	group_url = '/api/v2/groups'
 	search_url = '/api/v2/search?q='
 
 	/* Did we find any VMs in the Bus app? */
-	if (static_filter_ba_vms.length > 0) {
+	if (ba_vms.length > 0) {
 		group_name = "BusApp_VMs_"+ba_name
 		vms_group_body = {
 			"isStatic": true,
 			"displayName": group_name,
-			"memberUuidList": static_filter_ba_vms,
+			"memberUuidList": ba_vms,
 			"criteriaList": [],
 			"groupType": "VirtualMachine"
 		}
 		
-		/* Does a group with the given name already exist? */
+		/* Either create a new group of Bus App VMs or update the existing group if it already exists. */
 		fetch(search_url+group_name)
 		.then(
 			function(response) {
@@ -88,27 +86,30 @@ fetch('/vmturbo/rest/search/?q=&scopes='+scope_id).then(res => {
 				response.json().then(
 					function(data) {
 						if (data.length > 0) {
+							/* Found an existing group, so update it. */
 							CreateUpdate_Group(group_url+"/"+data[0].uuid, 'PUT', vms_group_body) 
+							console.log("Updated VM Group: "+group_name)
 						} else {
+							/* Create a new group */
 							CreateUpdate_Group(group_url, 'POST', vms_group_body) 
+							console.log("Created VM Group: "+group_name)
 						}
 				});
 		});
-		
-		console.log("Created VM Group: "+group_name)
 	}
 
-	if (static_filter_ba_dbs.length > 0) {
+	/* Did we find any DB servers in the Bus app? */
+	if (ba_dbs.length > 0) {
 		group_name = "BusApp_DBs_"+ba_name
 		dbs_group_body = {
 			"isStatic": true,
 			"displayName": group_name,
-			"memberUuidList": static_filter_ba_dbs,
+			"memberUuidList": ba_dbs,
 			"criteriaList": [],
 			"groupType": "DatabaseServer"
 		}
 		
-		/* Does a group with the given name already exist? */
+		/* Either create a new group of Bus App DBs or update the existing group if it already exists. */
 		fetch(search_url+group_name)
 		.then(
 			function(response) {
@@ -119,15 +120,16 @@ fetch('/vmturbo/rest/search/?q=&scopes='+scope_id).then(res => {
 				response.json().then(
 					function(data) {
 						if (data.length > 0) {
+							/* Found an existing group, so update it. */
 							CreateUpdate_Group(group_url+"/"+data[0].uuid, 'PUT', dbs_group_body) 
+							console.log("Updated DB Server Group: "+group_name)
 						} else {
+							/* Create a new group. */
 							CreateUpdate_Group(group_url, 'POST', dbs_group_body) 
+							console.log("Created DB Server Group: "+group_name)
 						}
 				});
 		});
-		
-		console.log("Created DB Server Group: "+group_name)
-
 	}
 	
 });
