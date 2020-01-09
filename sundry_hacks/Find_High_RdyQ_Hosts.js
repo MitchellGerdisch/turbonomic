e00a3f529663e88dd9cd0e33bd0053e2a2d107f0
MitchellGerdisch <mitchell.gerdisch@jesslex.com>
2020-01-09 15:37:12 -0600

    added some intelligence to ignore less worrisome VMs.

--- sundry_hacks/Find_High_RdyQ_Hosts.js
+++ sundry_hacks/Find_High_RdyQ_Hosts.js
@@ -1,52 +1,28 @@
-Walk through VMs
-- scoped to on-prem
-- further scoping parameter?
-Look at capacity and usage stats for each VM and find which readyQ it uses
-Look at 30 days? 7 days? 24 hours? of the identified readyQ and look for the largest number and report that number
-
 /*
- * Find high CPU ReadyQ VMs.
+ * Description:
+ * Finds VMs with high CPU Ready Queue over the recent past.
+ * 
+ * Usage:
+ *   - Copy paste the contents of this file into a Chrome javascript console.
+ *   - At the Chome javascript console prompt enter "findHighRdyqVMs()"
+ *   - Wait for the processing to complete and a list of VM names and related data to be presented.
+ *     The most interesting examples should be at the bottom of the list.
  */
-async function FindHighRdyQ(param) {
-	if ((param == null) || (param == "")) { 
-		/* go through all on-premise VMs */
-		vm_rdyq_types = await getVM_rdyQ_types()
-		console.log("******")
-		console.log(JSON.stringify(vm_rdyq_types))
-		console.log("*****")
-	} 
-/* else if (param == "all") {
-		console.log("Creating VM/DB groups for each Business Application found in this Turbo instance.")
-		busappslist = await getBusAppsList()
-		if (busappslist.length > 0) {
-			for (i = 0; i < busappslist.length; i++) {
-				item = busappslist[i]
-				await BuildBusAppGroup(item.uuid)
-				console.log("")
-			}
-		} else {
-			console.log("No Business Applications Found.")
-		}
-
-	} else {
-		busappname = param
-		busapp_uuid = await getUuid("BusApp", busappname) 
-		if (busapp_uuid) {
-			console.log("Building VM/DB group for Business Application, "+busappname) 
-			await BuildBusAppGroup(busapp_uuid)
-		} else {
-			console.log("NOT FOUND: Business App, "+busappname)
-		}
-	}
-*/
-}
-
-async function findHighRdyqVMs() {
+async function findHighRdyqVMs() { 
+	
+	console.log("Search for high ready queue VMs has started ...")
 	/*
 	 * Get all the on-prem VMs.
 	 * A later enhancement may allow one to scope the search.
 	 */
-	onprem_vms = await getOnpremVMs()
+	onprem_vms = await getVMs()
+	
+	if (onprem_vms.length == 0) {
+		console.log("No on premise VMs found. Exiting.")
+		return
+	}
+	
+	console.log("Found "+onprem_vms.length+" on premise VMs. Scanning their ready queue histories ...")
 
 	
 	/* Walk through the list of found VMs and do the following:
@@ -58,8 +34,7 @@ async function findHighRdyqVMs() {
 	/* 
 	 * Currently set to look over the last 7 days. 
 	 */ 
-	days_back = 3
-	/*days_back = 7*/
+	days_back = 7
 	milliseconds_per_day = 86400*1000
 	milliseconds_in_the_past = milliseconds_per_day * days_back
 	end_time = Date.now()
@@ -72,33 +47,39 @@ async function findHighRdyqVMs() {
 	
 	for (i = 0; i < onprem_vms.length; i++) { 
 		onprem_vm = onprem_vms[i]
-		vm_name = onprem_vm.displayName
-		vm_uuid = onprem_vm.uuid
-		vm_cpunum = onprem_vm.aspects.virtualMachineAspect.numVCPUs
-		vm_rdyq_type = "Q"+vm_cpunum+"VCPU"
 		
-		/* 
-		 * Now for each VM, get the highest RdyQ utilization over the past X days.
-		 */
-		vm_high_rdyq = await get_High_RdyQ(vm_uuid, vm_rdyq_type, start_time, end_time)
-
-		/* See: https://communities.vmware.com/docs/DOC-11494
-		 * The RdyQ utilization is divided by the number of CPUs to get the normalized value.
-		 */
-		norm_rdyq = Math.round(vm_high_rdyq/vm_cpunum)
-		
-		/* Store the info for interesting examples which I'm defining as those cases where the normalized number is greater than 4 */
-		if (norm_rdyq > 4) {
-			vm_rdyq_info.push({
-				"name":vm_name,
-				"rdyq":vm_rdyq_type,
-				"high_rdyq":vm_high_rdyq,
-				"norm_rdyq":norm_rdyq
-			})
+		/* To help the signal-to-noise ratio, we only bother looking at VMs that have a bit of severity to them. */
+		if ((onprem_vm.severity == "Critical")||(onprem_vm.severity == "Major") || (onprem_vm.severity == "Minor")) {
+			vm_name = onprem_vm.displayName
+			vm_uuid = onprem_vm.uuid
+			vm_cpunum = onprem_vm.aspects.virtualMachineAspect.numVCPUs
+			vm_rdyq_type = "Q"+vm_cpunum+"VCPU"
+			
+			/* 
+			 * Now for each VM, get the highest RdyQ utilization over the past X days.
+			 */
+			vm_high_rdyq = await get_High_RdyQ(vm_uuid, vm_rdyq_type, start_time, end_time)
+	
+			/* See: https://communities.vmware.com/docs/DOC-11494
+			 * The RdyQ utilization is divided by the number of CPUs to get the normalized value.
+			 */
+			norm_rdyq = Math.round(vm_high_rdyq/vm_cpunum)
+			
+			/* Store the info for interesting examples which I'm defining as those cases where the normalized number is greater than 4 */
+			if (norm_rdyq > 4) {
+				vm_rdyq_info.push({
+					"name":vm_name,
+					"rdyq":vm_rdyq_type,
+					"high_rdyq":vm_high_rdyq,
+					"norm_rdyq":norm_rdyq
+				})
+			}
 		}
 	}
 	
 	sorted_rdyq_info = _.sortBy(vm_rdyq_info, 'norm_rdyq')
+	
+	console.log("Here are VMs with interesting ready queue histories:")
 	for (k = 0; k < sorted_rdyq_info.length; k++) {
 		rdyq_item = sorted_rdyq_info[k]
 		console.log("VM Name: "+rdyq_item.name+"; RdyQ: "+rdyq_item.rdyq+"; High RdyQ: "+rdyq_item.high_rdyq+"; Normalized RdyQ: "+rdyq_item.norm_rdyq)
@@ -136,8 +117,11 @@ async function get_High_RdyQ(vm_uuid, vm_rdyq_type, start_time, end_time) {
 	return highest_rdyq_util
 }
 
-/* Returns list of on prem VMs */
-async function getOnpremVMs() {
+/* 
+ * Returns list of on prem VMs with severity of Major or Critical 
+ * This is done to allow the processing to focus on what should be useful examples.
+ */
+async function getVMs() {
 	request_body = {
 			"criteriaList": [],
 			"logicalOperator": "AND",
@@ -145,7 +129,7 @@ async function getOnpremVMs() {
 			"environmentType": "ONPREM",
 			"scope": null
 	}
-	response = await fetch('/vmturbo/rest/search', {
+	response = await fetch('/vmturbo/rest/search/?ascending=false&disable_hateoas=true&order_by=severity&q=', {
 		method: 'POST',
 		body: JSON.stringify(request_body),
 		headers: {
