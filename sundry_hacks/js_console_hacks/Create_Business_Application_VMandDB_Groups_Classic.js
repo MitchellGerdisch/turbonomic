882b3c49c0d26fda3ec7c7016972dc447b0dac2c
MitchellGerdisch <mitchell.gerdisch@jesslex.com>
2020-04-30 13:09:24 -0500

    XL-specific bus app group creator

--- sundry_hacks/js_console_hacks/Create_Business_Application_VMandDB_Groups_Classic.js
+++ sundry_hacks/js_console_hacks/Create_Business_Application_VMandDB_Groups_Classic.js
@@ -1,24 +1,26 @@
 /*
-* CAVEATS: 
-* 	This is a pretty much sunny day script. It doesn't do any error checking.
-* 	This script ONLY works for Business Applications.
-* 
-* How to Use:
-* First, copy the script into Chrome Javascript console.
-* Then you have three ways of running the script:
-* 1) Navigate to a scoped view of a given Business Application in the Turbo UI. I.e. Search -> Business Applications and select a given application.
-*    Then, enter "CreateBusAppGroups("scoped") to create VM and/or DB server groups for the given Business Application in view.
-* 2) Enter "CreateBusAppGroups("all") to create VM and/or DB server groups for every Business Application.
-*    Note: You do NOT need to be in a scoped view for this command.
-* 3) Enter "CreateBusAppGroups("<BUSINESS_APP_NAME>") to create VM and/or DB server groups for the given, named Business Application.
-*    Note: You do NOT need to be in a scoped view for this command.
-* 
-* 
-* What it Produces:
-* Up to two groups for the Business Application with a naming convention as follows:
-* - BusApp_VMs_<BUSINESS APP NAME>: A group that contains the VMs for that scoped view, assuming there are some.
-* - BusApp_DBs_<BUSINESS APP NAME>: A group that contains the DB servers for that scoped view, assuming there are some.
-*/
+ * NAME: Create_Business_Application_VMandDB_Groups_Classic
+ * 
+ * CAVEATS: 
+ * 	This is a pretty much sunny day script. It doesn't do any error checking.
+ * 	This script ONLY works for Business Applications.
+ * 
+ * How to Use:
+ * First, copy the script into Chrome Javascript console.
+ * Then you have three ways of running the script:
+ * 1) Navigate to a scoped view of a given Business Application in the Turbo UI. I.e. Search -> Business Applications and select a given application.
+ *    Then, enter "CreateBusAppGroups("scoped") to create VM and/or DB server groups for the given Business Application in view.
+ * 2) Enter "CreateBusAppGroups("all") to create VM and/or DB server groups for every Business Application.
+ *    Note: You do NOT need to be in a scoped view for this command.
+ * 3) Enter "CreateBusAppGroups("<BUSINESS_APP_NAME>") to create VM and/or DB server groups for the given, named Business Application.
+ *    Note: You do NOT need to be in a scoped view for this command.
+ * 
+ * 
+ * What it Produces:
+ * Up to two groups for the Business Application with a naming convention as follows:
+ * - BusApp_VMs_<BUSINESS APP NAME>: A group that contains the VMs for that scoped view, assuming there are some.
+ * - BusApp_DBs_<BUSINESS APP NAME>: A group that contains the DB servers for that scoped view, assuming there are some.
+ */
 
 
 
@@ -82,7 +84,6 @@ async function BuildBusAppGroup(scope_id) {
 	const group_name_keyword = "BusApp"
 
 	busappinfo = await getBusAppInfo(scope_id)
-	console.log("busappinfo: "+JSON.stringify(busappinfo))
 	if (busappinfo.length > 0) {
 		busappinfo.forEach(function (item, index) {
 			if (item.className == "BusinessApplication") {
@@ -168,27 +169,9 @@ async function CreateUpdate_Group(group_url, api_method, body) {
 
 /* Looks at current UI page and grabs the scope ID */
 async function getBusAppInfo(scope_id) {
-	console.log("scope: "+scope_id)
-	response = await fetch('/vmturbo/rest/search?q=&types=BusinessApplication&scopes='+scope_id)
+	response = await fetch('/vmturbo/rest/search/?q=&scopes='+scope_id)
 	return await response.json()
 }
-
-vmturbo/rest/search/?ascending=false&disable_hateoas=true&limit=20&order_by=severity&q=
-	
-	************** for VMs
-	https://10.30.172.17/vmturbo/rest/search/?ascending=false&aspect_names=cloudAspect&disable_hateoas=true&limit=20&order_by=severity&q=
-	{
-		  "criteriaList": [
-		    
-		  ],
-		  "logicalOperator": "AND",
-		  "className": "VirtualMachine",
-		  "environmentType": "HYBRID",
-		  "scope": [
-		    "284547951815392" /* UUID */
-		  ]
-		}
-***************
 
 /* Returns search list of Business Applications */
 async function getBusAppsList() {
--- sundry_hacks/js_console_hacks/Create_Business_Application_VMandDB_Groups_XL.js
+++ sundry_hacks/js_console_hacks/Create_Business_Application_VMandDB_Groups_XL.js
@@ -1,32 +1,23 @@
 /*
-* CAVEATS: 
-* 	This is a pretty much sunny day script. It doesn't do any error checking.
-* 	This script ONLY works for Business Applications.
-* 
-* How to Use:
-* First, copy the script into Chrome Javascript console.
-* Then you have three ways of running the script:
-* 1) Navigate to a scoped view of a given Business Application in the Turbo UI. I.e. Search -> Business Applications and select a given application.
-*    Then, enter "CreateBusAppGroups("scoped") to create VM and/or DB server groups for the given Business Application in view.
-* 2) Enter "CreateBusAppGroups("all") to create VM and/or DB server groups for every Business Application.
-*    Note: You do NOT need to be in a scoped view for this command.
-* 3) Enter "CreateBusAppGroups("<BUSINESS_APP_NAME>") to create VM and/or DB server groups for the given, named Business Application.
-*    Note: You do NOT need to be in a scoped view for this command.
-* 
-* 
-* What it Produces:
-* Up to two groups for the Business Application with a naming convention as follows:
-* - BusApp_VMs_<BUSINESS APP NAME>: A group that contains the VMs for that scoped view, assuming there are some.
-* - BusApp_DBs_<BUSINESS APP NAME>: A group that contains the DB servers for that scoped view, assuming there are some.
-*/
-
-
-
-/* Main function called to build groups 
- * Usage:
- * - CreateBusAppGroups("scoped"): Produces VM/DB group(s) for Business Application currently scoped in the Turbo UI view.
- * - CreateBusAppGroups("all"): Produces VM/DB groups for ALL Business Applications known by the Turbo instance.
- * - CreateBusAppGroups("<NAME_OF_BUSINESS_APP>"): Produces VM/DB group(s) for named Business Application
+ * NAME: Create_Business_Application_VMandDB_Groups_Classic
+ * 
+ * CAVEATS: 
+ * 	This script ONLY works for Business Applications.
+ *  You may see a 400 error when running the script. If the script outputs a message that you can ignore the 400 error, then do just that.
+ * 
+ * How to Use:
+ * First, copy the script into Chrome Javascript console.
+ * Then you have two ways of running the script:
+ * 1) Enter "CreateBusAppGroups("all") to create VM and/or DB server groups for every Business Application.
+ *    Note: You do NOT need to be in a scoped view for this command.
+ * 2) Enter "CreateBusAppGroups("<BUSINESS_APP_NAME>") to create VM and/or DB server groups for the given, named Business Application.
+ *    Note: You do NOT need to be in a scoped view for this command.
+ * 
+ * 
+ * What it Produces:
+ * Up to two groups for the Business Application with a naming convention as follows:
+ * - BusApp_VMs_<BUSINESS APP NAME>: A group that contains the VMs for that scoped view, assuming there are some.
+ * - BusApp_DBs_<BUSINESS APP NAME>: A group that contains the DB servers for that scoped view, assuming there are some.
  */
 
 /*
@@ -34,31 +25,23 @@
  */
  console.log("")
  console.log("**** USAGE ****")
- console.log("  CreateBusAppGroups(\"scoped\"): Produces VM/DB group(s) for Business Application currently scoped in the Turbo UI view.")
- console.log("  CreateBusAppGroups(\"all\"): Produces VM/DB groups for ALL Business Applications known by the Turbo instance.")
- console.log("  CreateBusAppGroups(\"<NAME_OF_BUSINESS_APP>\"): Produces VM/DB group(s) for named Business Application.")
+ console.log("CreateBusAppGroups(\"all\"): Produces VM/DB groups for ALL Business Applications known by the Turbo instance.")
+ console.log("CreateBusAppGroups(\"<NAME_OF_BUSINESS_APP>\"): Produces VM/DB group(s) for named Business Application.")
  console.log("")
 
 
 async function CreateBusAppGroups(param) {
-	if ((param == null) || (param == "") || (param == "scoped")) {
-		var thisurl = document.URL;
-		/* console.log("URL: "+thisurl); */
-		var url_array = thisurl.split('/');
-		var scope_id = url_array[url_array.length-1];
-		/* console.log("SCOPE ID: "+scope_id); */
-		console.log("Building VM/DB group for Business Application in current scoped view.")
-		BuildBusAppGroup(scope_id)
-		
-	} else if (param == "all") {
+	if (param == "all") {
 		console.log("Creating VM/DB groups for each Business Application found in this Turbo instance.")
 		busappslist = await getBusAppsList()
 		if (busappslist.length > 0) {
 			for (i = 0; i < busappslist.length; i++) {
 				item = busappslist[i]
-				await BuildBusAppGroup(item.uuid)
+				await BuildBusAppGroup(item.displayName, item.uuid)
 				console.log("")
 			}
+			console.log("")
+			console.log("*** DONE ***")
 		} else {
 			console.log("No Business Applications Found.")
 		}
@@ -68,43 +51,26 @@ async function CreateBusAppGroups(param) {
 		busapp_uuid = await getUuid("BusApp", busappname) 
 		if (busapp_uuid) {
 			console.log("Building VM/DB group for Business Application, "+busappname) 
-			await BuildBusAppGroup(busapp_uuid)
+			await BuildBusAppGroup(busappname, busapp_uuid)
+			console.log("")
+			console.log("*** DONE ***")
 		} else {
 			console.log("NOT FOUND: Business App, "+busappname)
 		}
 	}
 }
 
-async function BuildBusAppGroup(scope_id) {
-	var ba_name = "BA NAME NOT FOUND"
-	var ba_vms = []
-	var ba_dbs = []
+async function BuildBusAppGroup(ba_name, busapp_uuid) {
 	const group_name_keyword = "BusApp"
+	console.log("*** Business Application Name: "+ba_name)
 
-	busappinfo = await getBusAppInfo(scope_id)
-	console.log("busappinfo: "+JSON.stringify(busappinfo))
-	if (busappinfo.length > 0) {
-		busappinfo.forEach(function (item, index) {
-			if (item.className == "BusinessApplication") {
-				ba_name = item.displayName
-			} else if (item.className == "VirtualMachine") {
-				ba_vms.push(item.uuid)
-			} else if (item.className == "DatabaseServer") {
-				ba_dbs.push(item.uuid)
-			}
-		})
-	} else {
-		console.log("No data found for Business Application with UUID: "+scope_id)
-	}
-
-	console.log("Business Application Name: "+ba_name)
+	var ba_vms = await getBusAppElements("VM",busapp_uuid)
 	console.log("VMs in Bus App: " + ((ba_vms.length > 0) ? ba_vms : "NONE FOUND"))
+
+	var ba_dbs = await getBusAppElements("DB",busapp_uuid)
 	console.log("DB Servers in Bus App: " + ((ba_dbs.length > 0) ? ba_dbs : "NONE FOUND"))
 	
 	group_url = '/vmturbo/rest/groups'
-	/*group_url = '/api/v2/groups'*/
-	/*search_url = '/api/v2/search?q='*/
-
 	/* Did we find any VMs in the Bus app? */
 	if (ba_vms.length > 0) {
 		group_name = group_name_keyword+"_VMs_"+ba_name
@@ -153,8 +119,65 @@ async function BuildBusAppGroup(scope_id) {
 			console.log("Created DB Group: "+group_name)
 		}
 	}
+}
+	
+/* Returns search list of Business Applications */
+async function getBusAppsList() {
+	/*search_url = '/api/v2/search?types=BusinessApplication'*/
+	search_url = '/vmturbo/rest/search?types=BusinessApplication'
+	response = await fetch(search_url)
+	return await response.json()
+}
+
+/*
+ * Turbo may return a 400 if the given business app doesn't have any elements of the given type.
+ * For example, a business app with no DB servers will cause a 400 error from Turbo.
+ * This function catches the problem and continues processing.
+ * BUT, the browser may show the 400 error and it should be ignored.
+ * 
+ * Returns an array of UUIDs of VMs or DBs associated with the UUID.
+ */
+async function getBusAppElements(element_type, uuid) {
+	search_body = {
+			"criteriaList": [],
+			"logicalOperator": "AND",
+			"className": (element_type == "DB" ? "DatabaseServer" : "VirtualMachine"), 
+			"environmentType": "HYBRID",
+			"scope": [uuid]
+	}
+	elements_info = []
+	elements = []
+	
+	await fetch('/vmturbo/rest/search', {
+		method: 'POST',
+		body: JSON.stringify(search_body),
+		headers: {
+		'Content-Type': 'application/json'
+		}
+	})
+	/* Check if we got anything back. */
+	.then(function(response) {
+			if (!response.ok) {
+				console.log("(Ignore 400 error.)")
+				throw Error("no elements found")
+			}
+			return response /* if the throw doesn't fire, forward the response to the success case processing */
+	/* Process the success case */
+	}).then(async function(response) {
+			elements_info = await response.json()
+			if (elements_info.length > 0) {
+				elements_info.forEach(function (item, index) {
+					elements.push(item.uuid) 
+				})
+			}
+	})
+	.catch(function(err) {
+		/* do nothing */
+	})
+	return elements
 } 
 
+/* wrapper for API call to create or update a group */
 async function CreateUpdate_Group(group_url, api_method, body) {
 	
 	response = await fetch(group_url, {
@@ -165,38 +188,6 @@ async function CreateUpdate_Group(group_url, api_method, body) {
 	      }
 	})
 } 
-
-/* Looks at current UI page and grabs the scope ID */
-async function getBusAppInfo(scope_id) {
-	console.log("scope: "+scope_id)
-	response = await fetch('/vmturbo/rest/search?q=&types=BusinessApplication&scopes='+scope_id)
-	return await response.json()
-}
-
-vmturbo/rest/search/?ascending=false&disable_hateoas=true&limit=20&order_by=severity&q=
-	
-	************** for VMs
-	https://10.30.172.17/vmturbo/rest/search/?ascending=false&aspect_names=cloudAspect&disable_hateoas=true&limit=20&order_by=severity&q=
-	{
-		  "criteriaList": [
-		    
-		  ],
-		  "logicalOperator": "AND",
-		  "className": "VirtualMachine",
-		  "environmentType": "HYBRID",
-		  "scope": [
-		    "284547951815392" /* UUID */
-		  ]
-		}
-***************
-
-/* Returns search list of Business Applications */
-async function getBusAppsList() {
-	/*search_url = '/api/v2/search?types=BusinessApplication'*/
-	search_url = '/vmturbo/rest/search?types=BusinessApplication'
-	response = await fetch(search_url)
-	return await response.json()
-}
 
 /* Returns UUID for named entity of given type */
 async function getUuid(entity_type, entity_name) {
